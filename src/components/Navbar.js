a18169880fe6578f59efb08f5aadaab125001fb4
PulindaDissanayake <dpulinda@gmail.com>
2021-01-06 08:42:28 +0530

    Updated with Router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,25 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                 <Link to="/" className="navbar-logo">
+                     FOODIES <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+                 </Link>
+                 <div className='menu-icon' onClick ={handleClick}S>
+                     <i className={click ? 'fas fa-times' :'fas fa-bars'} />
+                 </div>
+                </div>  
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
